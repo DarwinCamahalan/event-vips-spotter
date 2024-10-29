b2a58645e8eddd9062a93d5b93361d44a6cf2931
DarwinCamahalan <camahalandarwin@gmail.com>
2024-10-29 15:26:36 +0800

    Deployed App to Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Display from "./components/Display";
 import Spotter from "./components/Spotter";
 import Settings from "./components/Settings";
@@ -10,9 +15,15 @@ function App() {
     <Router>
       <div>
         <Routes>
+          {/* Redirect root path "/" to "/settings" */}
+          <Route path="/" element={<Navigate to="/settings" replace />} />
+
+          {/* Define other routes */}
           <Route path="/display" element={<Display />} />
           <Route path="/spotter" element={<Spotter />} />
           <Route path="/settings" element={<Settings />} />
+
+          {/* Fallback for unmatched routes */}
           <Route path="*" element={<div>Page not found</div>} />
         </Routes>
       </div>

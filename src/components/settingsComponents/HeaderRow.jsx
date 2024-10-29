e02d60fa525c0d27fa640f5c8d2178458c88cd24
DarwinCamahalan <camahalandarwin@gmail.com>
2024-10-29 10:24:32 +0800

    Added Display Button in Settings

--- src/components/settingsComponents/HeaderRow.jsx
+++ src/components/settingsComponents/HeaderRow.jsx
@@ -1,17 +1,30 @@
 // HeaderRow.jsx
 import React from "react";
-import { FaUserCheck } from "react-icons/fa";
+import { FaUserCheck, FaTv } from "react-icons/fa"; // Import icons
 
 const HeaderRow = ({ commonPadding }) => {
   return (
-    <div className="flex justify-between items-center mb-4 ">
+    <div className="flex justify-between items-center mb-4 space-x-2">
       <h2 className="text-2xl font-bold">Settings</h2>
-      <button
-        onClick={() => (window.location.href = "/spotter")}
-        className={`flex items-center bg-green-800 text-white ${commonPadding} rounded hover:bg-green-700`}
-      >
-        <FaUserCheck className="mr-2" /> Spot Attendees
-      </button>
+
+      {/* Button container - side by side on all screen sizes */}
+      <div className="flex space-x-2">
+        {/* View Display Button */}
+        <button
+          onClick={() => (window.location.href = "/display")}
+          className={`flex items-center justify-center bg-gray-800 text-white ${commonPadding} rounded hover:bg-gray-700`}
+        >
+          <FaTv className="mr-2" /> Display
+        </button>
+
+        {/* Spot Attendees Button */}
+        <button
+          onClick={() => (window.location.href = "/spotter")}
+          className={`flex items-center justify-center bg-green-800 text-white ${commonPadding} rounded hover:bg-green-700`}
+        >
+          <FaUserCheck className="mr-2" /> Spotter
+        </button>
+      </div>
     </div>
   );
 };

09c1f0bd0f31f3c56717b1c009c62ceb5c2a434a
DarwinCamahalan <camahalandarwin@gmail.com>
2024-11-10 16:13:58 +0800

    Change Notification Color

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -259,6 +259,7 @@ const Settings = () => {
             title={modalTitle}
             description={modalDescription}
             onClose={() => setModalVisible(false)}
+            type={"info"}
           />
         )}
 
